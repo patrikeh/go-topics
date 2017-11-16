4acf45f8e03c51869e88d61c3eb992e25c6c59e6
patrikeh <patrik.ehrencrona@recaresolutions.com>
2017-11-16 21:54:35 +0100

    concmap

---
+++ concurrentMap.go
@@ -0,0 +1,33 @@
+package words
+
+import "sync"
+
+type Map struct {
+	sync.RWMutex
+	internal map[int]interface{}
+}
+
+func NewMap() *Map {
+	return &Map{
+		internal: make(map[int]interface{}),
+	}
+}
+
+func (m *Map) Load(key int) (interface{}, bool) {
+	m.RLock()
+	defer m.RUnlock()
+	value, found := m.internal[key]
+	return value, found
+}
+
+func (m *Map) Delete(key int) {
+	m.Lock()
+	defer m.Unlock()
+	delete(m.internal, key)
+}
+
+func (m *Map) Store(key int, value interface{}) {
+	m.Lock()
+	defer m.Unlock()
+	m.internal[key] = value
+}
