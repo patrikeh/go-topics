0d2ff98027566cc399d67127b6850166cfbfbffc
patrikeh <patrikeh@gmail.com>
2017-12-06 20:08:52 +0100

    remove twitter stuff

--- processor.go
+++ processor.go
@@ -2,18 +2,10 @@ package words
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"os"
-	"regexp"
 	"strings"
 )
-
-type Transformation func(word string) (new string, keep bool)
-type Transformations []Transformation // i.e. stemming, lemmatization, etc
-type Processor struct {
-	transformations Transformations
-}
 
 func NewDefaultProcessor() *Processor {
 	return NewProcessor(
@@ -109,53 +101,4 @@ func (p *Processor) importSingleFileCorpus(path string) ([]string, error) {
 		lines = append(lines, scanner.Text())
 	}
 	return lines, scanner.Err()
-}
-
-func GetStopwordFilter(path string) Transformation {
-	f, err := os.Open(path)
-	defer f.Close()
-	if err != nil {
-		fmt.Printf("%+v", err)
-		return nil
-	}
-
-	s := bufio.NewScanner(f)
-
-	stopWords := make(map[string]bool, 0)
-	for s.Scan() {
-		stopWords[s.Text()] = true
-	}
-
-	return func(stopWords map[string]bool) Transformation {
-		return func(w string) (string, bool) {
-			if stopWords[w] {
-				return "", false
-			}
-			return w, true
-		}
-	}(stopWords)
-}
-
-func ToLower(w string) (string, bool) {
-	return strings.ToLower(w), true
-}
-
-var wordReg = regexp.MustCompile("[a-zA-Z'åäöÅÄÖ]+")
-
-func Sanitize(w string) (string, bool) {
-	return strings.TrimSpace(wordReg.FindString(w)), true
-}
-
-func MinLen(w string) (string, bool) {
-	if len(w) < 2 {
-		return "", false
-	}
-	return w, true
-}
-
-func RemoveTwitterUsernames(w string) (string, bool) {
-	if strings.Contains(w, "@") {
-		return "", false
-	}
-	return w, true
 }
